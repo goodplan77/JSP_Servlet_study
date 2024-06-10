8b0906cf7446204ba55be1333057bc2017d7ffed
goodplan77 <goodplan77@naver.com>
2024-06-10 19:30:29 +0900

    MVC Practice

--- 04_mvc/src/main/java/com/kh/member/controller/MemberInsertController.java
+++ 04_mvc/src/main/java/com/kh/member/controller/MemberInsertController.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/member/insert")
 public class MemberInsertController extends HttpServlet {
@@ -23,6 +24,18 @@ public class MemberInsertController extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		System.out.println("post 진입");
+		HttpSession session = request.getSession();
+		System.out.println((String)request.getParameter("userId"));
+		System.out.println((String)request.getParameter("userPwd"));
+		System.out.println((String)request.getParameter("userName"));
+		System.out.println((String)request.getParameter("phone"));
+		System.out.println((String)request.getParameter("email"));
+		System.out.println((String)request.getParameter("address"));
+		String[] arg = request.getParameterValues("interest");
+		for(String s : arg) {
+			System.out.println(s);
+		}
 	}
 
 }
